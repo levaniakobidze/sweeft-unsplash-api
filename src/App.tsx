3cc4fb37918336dd401deed53c66395a253cadf1
levaniakobidze <levaniakobidze25@gmail.com>
2024-02-28 20:56:03 +0400

    Setup:(Libraries): setup react-query and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Hello Sweeft</h1>
+      <Routes>
+        <Route />
+      </Routes>
     </>
   );
 }
