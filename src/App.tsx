cf635fc5a9506984c7dab003afd2c3f0e4f29336
levaniakobidze <levaniakobidze25@gmail.com>
2024-02-28 22:06:50 +0400

    feature:(Home): home page basic markup and styleing finished for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import History from "./pages/History/History";
 import Header from "./components/Header/Header";
-import Layout from "./components/Layout/Layout";
 
 const App: FC = () => {
   return (
     <Fragment>
       <Header />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </Fragment>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,29 @@
+import Layout from "../../components/Layout/Layout";
 import classes from "../../styles/Home.module.css";
 
 const Home = () => {
-  return <div className={classes.home}>Home</div>;
+  return (
+    <div className={classes.home}>
+      <div className={classes.banner}>
+        <div className={classes.hero_cont}>
+          <Layout>
+            <div className={classes.hero}>
+              <p className={classes.sub_title}>WALLPAPERS</p>
+              <h1 className={classes.title}>The best wallpapers</h1>
+              <p className={classes.description}>
+                Maecenas volutpat consequat orci id consectetur. Namvulput quam
+                turpis, id tristique nibh ullamcorper dignissim.
+              </p>
+
+              <div className={classes.search_cont}>
+                <input type="text" placeholder="Search for the best images" />
+              </div>
+            </div>
+          </Layout>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
